e325d5afb046a73a1e8adce514d4451fc5baeb7d
Ashwin Kumar <realashwinkumar@gmail.com>
2017-11-04 16:52:50 -0400

    Add files via upload

---
+++ app/src/main/java/com/tommymathews/slackersguidetohealth/service/FitnessService.java
@@ -0,0 +1,13 @@
+package com.tommymathews.slackersguidetohealth.service;
+
+import com.tommymathews.slackersguidetohealth.model.Fitness;
+
+import java.util.List;
+
+public interface FitnessService {
+    public Fitness getFitnessById( String id );
+    List<Fitness> getFitnessesByName(String name);
+    public List<Fitness> getFitnessesByBodyPart(int bodyPart);
+    public List<Fitness> getAllFitness();
+    public void addFitness(Fitness fitness);
+}
